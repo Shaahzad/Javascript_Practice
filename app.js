d9510cd3ffa9149277b68978a3c84913b3d3c736
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-09-29 18:05:55 +0500

    Done

---
+++ app.js
@@ -0,0 +1,113 @@
+
+// Q 1   var let const
+
+// var Name = "shahzad"
+// let Name = "shahzad"
+// const Name = "shahzad"
+
+// console.log(Name)
+// console.log("Window Global", window.Name)
+
+
+// function hello(){
+//     var Name = 'shahzad'
+//     console.log(Name)
+// }
+// hello()
+// console.log(Name)
+
+
+// if(true){
+//     var Name1 = "shahzad"
+//     let Name2 = "shahzad"
+//     const Name3 = "shahzad"
+//     console.log(Name2)
+//     console.log(Name3)
+// }
+
+// console.log(Name1)
+// console.log(Name2)
+// console.log(Name3)
+
+
+// var Name = 'shahzad'
+// let Name = 'shahzad'
+// const Name = 'shahzad'
+
+// function hello(){
+//     console.log(Name)
+//     console.log(window.Name)
+// }
+// hello()
+
+
+
+// Q2   Hoisting
+
+// console.log(Name)
+
+// var Name = "shahzad"
+
+// console.log(Name)
+
+// console.log(Mylet)
+
+// let Mylet = 'shahzad'
+
+// console.log(Mylet)
+
+// hello()
+// function hello(){
+//     console.log("first")
+// }
+ 
+// myfunction()
+// var myfunction = function(){
+//     console.log("first")
+// }
+// myfunction()
+
+// Q3   Difference between == and ===
+
+
+// let a = undefined
+// let b = null
+// console.log(a == b)
+// console.log(a === b)
+
+
+// let a = true
+// let b = false
+// console.log(a == b)
+// console.log(a === b)
+
+
+// let a = "5"
+// let b =  5
+// console.log(a == b)
+// console.log(a === b)
+
+// let a = 1
+// let b = true
+// console.log(a == b)
+// console.log(a === b)
+
+
+
+
+//  Basics 
+//  Explain var, let, and const. How are they different?  ✅
+// What is hoisting in JavaScript? ✅
+// What is the difference between == and ===? ✅
+// What is the purpose of arrow functions (=>) in JavaScript? How do they differ from regular functions?
+// Explain closures in JavaScript with an example.
+// What is the difference between null and undefined?
+// How does the JavaScript event loop work?
+// What are promises, and how do you handle asynchronous code in JavaScript?
+// What is the difference between map(), filter(), and reduce()?
+// Explain how this keyword works in JavaScript. How does it behave in different contexts?
+// What is the purpose of strict mode in JavaScript, and how do you enable it?
+// What are JavaScript template literals, and how do they work?
+// Explain the concept of prototypal inheritance in JavaScript.
+// What is the purpose of the async/await syntax in JavaScript?
+// How do you handle errors in JavaScript using try/catch blocks?
