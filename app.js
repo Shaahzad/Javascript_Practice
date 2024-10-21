e10049ac2a6eb883fee6f45d6c32a6748cd6e641
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-10-21 21:37:12 +0500

    Done

--- app.js
+++ app.js
@@ -2299,3 +2299,71 @@
 // animals[cat] = { ...cat, name: "Sara" }
 
 // console.log(animals[dog])
+
+
+// Q151
+
+// const user = {
+// 	email: "my@email.com",
+// 	updateEmail: email => {
+// 		this.email = email
+// 	}
+// }
+
+// user.updateEmail("new@email.com")
+// console.log(user.email)
+
+
+// Q152
+
+// const promise1 = Promise.resolve('First')
+// const promise2 = Promise.resolve('Second')
+// const promise3 = Promise.reject('Third')
+// const promise4 = Promise.resolve('Fourth')
+
+// const runPromises = async () => {
+// 	const res1 = await Promise.all([promise1, promise2])
+// 	const res2  = await Promise.all([promise3, promise4])
+// 	return [res1, res2]
+// }
+
+// runPromises()
+// 	.then(res => console.log(res))
+// 	.catch(err => console.log(err))
+
+
+// Q153
+
+// const keys = ["name", "age"]
+// const values = ["Lydia", 22]
+
+// const method = "fromEntries"
+// console.log(Object[method](keys.map((_, i) => {return [keys[i], values[i]]})))
+
+
+// Q154
+
+// const createMember = ({ email, address = {}}) => {
+// 	const validEmail = /.+\@.+\..+/.test(email)
+// 	if (!validEmail) throw new Error("Valid email pls")
+
+// 	return {
+// 		email,
+// 		address: address ? address : null
+// 	}
+// }
+
+// const member = createMember({ email: "my@email.com" })
+// console.log(member)
+
+
+// Q155
+
+// let randomValue = { name: "Lydia" }
+// randomValue = 23
+
+// if (!typeof randomValue === "string") {
+// 	console.log("It's not a string!")
+// } else {
+// 	console.log("Yay it's a string!")
+// }
