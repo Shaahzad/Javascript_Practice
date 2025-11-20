0ab2435d735fdcee2c2acf58ef58a1ef5bbc985c
Shaahzad <shehzadmemon147@gmail.com>
2025-11-20 09:31:35 +0500

    learn DSA Questions

--- app.js
+++ app.js
@@ -3488,3 +3488,20 @@
 
 
 // console.log(largestNumber([1,2,9,10]))
+
+
+
+// for (let index = 1; index <= 15; index++) {
+//   if (index % 3 === 0 && index % 5 === 0) {
+//     console.log("fizzbuzz")
+//   }  
+//   else if (index % 3 === 0) {
+//     console.log("fizz")
+//   }
+//   else if(index % 5 === 0) {
+//     console.log("buzz")
+//   }
+//   else{
+//     console.log(index)
+//   }
+// }
