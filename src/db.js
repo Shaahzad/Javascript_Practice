7141af7a1fbb0bc7fea5fc3ca49acc6deb93de00
Shaahzad <shehzadmemon147@gmail.com>
2026-01-06 17:22:56 +0500

    Practice

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connection = () => {
     try {
-        mongoose.connect("mongodb://127.0.0.1:27017/practiceDB");
+        mongoose.connect(process.env.MONGO_URL);
         console.log("DB connected");
     } catch (error) {
         console.log(error);
